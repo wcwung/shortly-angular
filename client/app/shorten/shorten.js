56ca274e3d2d1132d994fc2d5bdd396c93c58f4f
wcwung <williamcwung@gmail.com>
2015-03-07 17:13:40 -0800

    Add reference function to signout

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,10 +1,12 @@
 angular.module('shortly.shorten', [])
 
   // Your code here
-.controller('ShortenController', function ($scope, $window, $location, Links) {
+.controller('ShortenController', function ($scope, $window, $location, Links, Auth) {
 
   $scope.shorten = function () {
     $scope.url = $scope.shorten.url;
     return Links.postShortenLink({url: $scope.url});
   }
+
+  $scope.logout = Auth.signout;
 });
